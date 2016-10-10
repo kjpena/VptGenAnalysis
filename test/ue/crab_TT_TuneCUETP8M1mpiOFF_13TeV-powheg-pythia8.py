1248e11d8c76616c42405fb088fd97739ac33517
Pedro Silva <psilva@cern.ch>
2016-10-10 13:43:24 +0200

    renaming

---
+++ test/ue/crab_TT_TuneCUETP8M1mpiOFF_13TeV-powheg-pythia8.py
@@ -0,0 +1,25 @@
+from WMCore.Configuration import Configuration
+import os
+config = Configuration()
+
+config.section_("General")
+config.General.requestName = "TT_TuneCUETP8M1mpiOFF_13TeV-powheg-pythia8"
+config.General.workArea = "grid"
+config.General.transferOutputs=True
+
+config.section_("JobType")
+config.JobType.pluginName = "Analysis"
+config.JobType.psetName = "/afs/cern.ch/user/p/psilva/work/Wmass/CMSSW_8_0_8_patch1/src/UserCode/RivetAnalysis/test/runRivetModule_cfg.py"
+config.JobType.outputFiles = ['out.yoda']
+
+config.section_("Data")
+config.Data.inputDataset = "/TT_TuneCUETP8M1mpiOFF_13TeV-powheg-pythia8/RunIISpring16DR80-PUSpring16_80X_mcRun2_asymptotic_2016_v3-v1/AODSIM"
+config.Data.inputDBS = "global"
+config.Data.splitting = "FileBased"
+config.Data.unitsPerJob = 5
+config.Data.publication = False
+config.Data.ignoreLocality = True
+config.Data.outLFNDirBase = '/store/group/phys_top/psilva/UE_2f64282/'
+
+config.section_("Site")
+config.Site.storageSite = "T2_CH_CERN"
--- test/ue/runRivet.sh
+++ test/ue/runRivet.sh
@@ -2,13 +2,15 @@
 
 WHAT=${1}
 
+a=(
+    TT_TuneCUETP8M1_13TeV-powheg-pythia8
+    TT_TuneCUETP8M1noCR_13TeV-powheg-pythia8
+    TT_TuneEE5C_13TeV-powheg-herwigpp
+    TT_TuneCUETP8M1mpiOFF_13TeV-powheg-pythia8
+)
+
 case $WHAT in 
     RIVETSUBMIT )
-	a=(
-	    TT_TuneCUETP8M1_13TeV_powheg_pythia8
-	    TT_TuneCUETP8M1noCR_13TeV_powheg_pythia8
-	    TT_TuneEE5C_13TeV_powheg_herwigpp
-	)
 	for i in ${a[@]}; do
 	    crab submit crab_${i}.py
 	done	
@@ -20,9 +22,6 @@ case $WHAT in
 	/afs/cern.ch/project/eos/installation/0.3.15/bin/eos.select -b fuse mount eos;
 
 	#loop over directories of interest
-	a=(TT_TuneCUETP8M1_13TeV-powheg-pythia8  
-	    TT_TuneCUETP8M1noCR_13TeV-powheg-pythia8
-	    TT_TuneEE5C_13TeV-powheg-herwigpp)
 	for i in ${a[@]}; do 
 	    b=(`ls eos/cms/store/group/phys_top/psilva/UE_2f64282/${i}/*/*/*/*.yoda`)
 	    toMerge=""
@@ -42,6 +41,7 @@ case $WHAT in
 	rivet-mkhtml -s -o ${outDir}/Powheg --times --config=data/CMS_TOP_13_007.plot \
 	    TT_TuneCUETP8M1_13TeV-powheg-pythia8.yoda:'PW+PY8 CUETP8M1' \
 	    TT_TuneCUETP8M1noCR_13TeV-powheg-pythia8.yoda:'PW+PY8 CUETP8M1noCR' \
+	    TT_TuneCUETP8M1mpiOFF_13TeV-powheg-pythia8.yoda:'PW+PY8 CUETP8M1mpiOFF' \
 	    TT_TuneEE5C_13TeV-powheg-herwigpp.yoda:'PW+HW++ EE5C'
 	;;
 
